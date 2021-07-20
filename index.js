56e619273bdc2f389135c3d38d7e162f7a238f6b
Sungoh Park <spark449062@gmail.com>
2021-07-20 10:38:44 -0600

    commit tree c6cbfdb3a94f4d639e7a1433b70cb2013bf7dd8c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+let locationHQ = 42;
+function distanceFromHqInBlocks(currentLocation) {
+    return Math.abs(currentLocation - locationHQ);
+}
+function distanceFromHqInFeet(currentLocation) {
+    return Math.abs(currentLocation - locationHQ)*264;
+}
+function distanceTravelledInFeet(beginning, destination) {
+    return Math.abs(beginning - destination)*264;
+}
+function calculatesFarePrice(beginning, destination) {
+    let distance = distanceTravelledInFeet(beginning, destination);
+    // console.log(distance);
+    if (distance <= 400) {
+        return 0;
+    }
+    else if (distance > 400 && distance <= 2000) {
+        return (distance - 400)*0.02;
+    }
+    else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    }
+    else if (distance > 2500) {
+        return "cannot travel that far";
+    }
+}
+
+// console.log(calculatesFarePrice(30,44));
+// console.log(calculatesFarePrice(30,44));
